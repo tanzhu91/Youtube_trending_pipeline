1e78d76dc86a2e3860ec0af5d08101ec04381aea
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-06-17 18:14:30 +0200

    s

--- models/staging/stg_youtube_trending.sql
+++ models/staging/stg_youtube_trending.sql
@@ -1,14 +1,10 @@
-{{ config(materialized='view') }}
-
-
-with source_data as (
-    select * from {{ source('youtube_trending', 'video_basic_audience') }}
-)
-
 select
     video_id,
-    creator_channel_id,
-    start_date,
-    country_code,
-    viewer_percentage
-from source_data
+    title,
+    channel_title,
+    cast(view_count as int64) as view_count,
+    cast(like_count as int64) as like_count,
+    cast(comment_count as int64) as comment_count,
+    published_at,
+    duration_seconds
+from {{ source('youtube_source', 'youtube_videos_raw') }}
