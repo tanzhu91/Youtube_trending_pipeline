9347c6bd60ee03676b13f25751c8b0451057a2ec
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-06-22 00:15:10 +0200

    s

--- models/marts/core/fct_video_daily_performance.sql
+++ models/marts/core/fct_video_daily_performance.sql
@@ -18,7 +18,7 @@ daily_stats as (
         count(*) as times_seen,
         count(distinct published_at) as publish_dates_count
     from base
-    group by video_id, title, channel_title, category_name, DATE(load_date)
+    group by video_id, title, channel_title, category_name, DATE(load_date), load_hour
 )
 
 select * from daily_stats
