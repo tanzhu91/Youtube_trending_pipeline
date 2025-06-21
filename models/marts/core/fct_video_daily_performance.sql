ca4ddc0796461dd4d86510dab98035d16a05f11d
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-06-22 00:26:18 +0200

    s

--- models/marts/core/fct_video_daily_performance.sql
+++ models/marts/core/fct_video_daily_performance.sql
@@ -10,15 +10,15 @@ daily_stats as (
         title,
         channel_title,
         category_name,
+        load_hour,
         DATE(load_date) as load_date,
-        FORMAT_TIMESTAMP('%H:%M', load_date) AS load_hour,
         max(view_count) as max_views,
         max(like_count) as max_likes,
         max(comment_count) as max_comments,
         count(*) as times_seen,
         count(distinct published_at) as publish_dates_count
     from base
-    group by video_id, title, channel_title, category_name, DATE(load_date), load_hour
+    group by video_id, title, channel_title, category_name, load_hour, DATE(load_date)
 )
 
 select * from daily_stats
