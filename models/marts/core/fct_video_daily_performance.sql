2879e2f29c182cd4312b48bd61fa59eb5f49d545
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-06-28 21:10:00 +0200

    s

--- models/marts/core/fct_video_daily_performance.sql
+++ models/marts/core/fct_video_daily_performance.sql
@@ -6,14 +6,11 @@ with base as (
 
 daily_stats as (
     select
-        DATE(load_date) as load_date,
-        EXTRACT(DAY FROM load_date) AS load_day,
         DATE(published_at) as published_at,
-        EXTRACT(DAY FROM published_at) AS publish_day,
         SUM(view_count) as sum_views,
         SUM(like_count) as sum_likes,
         SUM(comment_count) as sum_comments
     from base
-    group by DATE(load_date), load_day, load_hour, DATE(published_at), publish_day
+    group by 1
 )
 select * from daily_stats
