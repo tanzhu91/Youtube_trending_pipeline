ab4da910e9e6dc2147302959e281fd251342456c
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-07-01 23:06:05 +0200

    s

--- models/youtube_star_schema/dim_video.sql
+++ models/youtube_star_schema/dim_video.sql
@@ -3,6 +3,9 @@
     schema='youtube_star_schema'
 ) }}
 
+
+create or replace table `upheld-momentum-463013-v7`.`dbt_tdereli_youtube_star_schema`.`dim_video`
+
 SELECT
   DISTINCT video_id,
   title,
