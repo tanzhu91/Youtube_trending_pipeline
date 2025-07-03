131cbfc0d932c2b28ae05f34c483fba5d4db5475
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-07-03 23:20:58 +0200

    s

--- models/youtube_star_schema/fact_video_metrics.sql
+++ models/youtube_star_schema/fact_video_metrics.sql
@@ -11,6 +11,6 @@ SELECT
   DATE(load_date) AS load_date,
   DATE_DIFF(DATE(load_date), DATE(published_at), DAY) AS trending_time,
   view_count AS views,
-  like_count AS ikes,
+  like_count AS likes,
   comment_count AS comments
 FROM {{ ref('stg_youtube_trending') }}
