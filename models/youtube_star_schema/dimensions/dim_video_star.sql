65da404c46fe52bd3f23f96046ece98226947744
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-07-05 00:15:44 +0200

    s

--- models/youtube_star_schema/dimensions/dim_video_star.sql
+++ models/youtube_star_schema/dimensions/dim_video_star.sql
@@ -6,9 +6,13 @@
 
 
 SELECT
-  DISTINCT video_id,
+  title,
+  video_id,
   published_at,
   load_date,
-  DATE_DIFF(DATE(load_date), DATE(published_at), DAY) AS days_until_it_became_trending
+  DATE_DIFF(DATE(load_date), DATE(published_at), DAY) AS days_until_it_became_trending,
+  description,
+  default_language,
+  tags
 FROM {{ ref('stg_youtube_trending') }}
 group by 1,2,3
