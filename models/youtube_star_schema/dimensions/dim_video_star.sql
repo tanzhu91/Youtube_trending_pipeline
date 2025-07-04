c1cf513bfa591a73c876e17ca01a36bd96013bcd
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-07-05 00:20:38 +0200

    s

--- models/youtube_star_schema/dimensions/dim_video_star.sql
+++ models/youtube_star_schema/dimensions/dim_video_star.sql
@@ -6,11 +6,8 @@
 
 
 SELECT
-  title,
   video_id,
-  published_at,
-  load_date,
-  DATE_DIFF(DATE(load_date), DATE(published_at), DAY) AS days_until_it_became_trending,
+  title,
   description,
   default_language,
   tags
