251e2aa35323f3d354cafebd642544dd90f23d61
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-07-04 12:41:25 +0200

    s

--- models/youtube_star_schema/dim_channel_star.sql
+++ models/youtube_star_schema/dim_channel_star.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='view', schema='youtube_star_schema') }}
 
 SELECT DISTINCT
-  channel_id,        
+  title,        
   channel_title
-FROM {{ ref('channel_info_enriched') }}
+FROM {{ ref('stg_youtube_trending') }}
