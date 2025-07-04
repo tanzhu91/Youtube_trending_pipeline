717cbd1dd365ee96c85344402ed987370601ca61
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-07-04 12:00:28 +0200

    s

--- models/youtube_star_schema/dim_video_star.sql
+++ models/youtube_star_schema/dim_video_star.sql
@@ -9,6 +9,6 @@ SELECT
   DISTINCT video_id,
   published_at,
   load_date,
-  DATE_DIFF(MAX(DATE(load_date)), DATE(published_at), DAY) AS days_until_it_became_trending
+  DATE_DIFF(DATE(load_date), DATE(published_at), DAY) AS days_until_it_became_trending
 FROM {{ ref('stg_youtube_trending') }}
 group by 1,2,3
