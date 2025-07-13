53fb98d317890111d9cd4c8edec38cbe8a6d645f
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-07-13 19:34:53 +0200

    s

--- models/prep/prep_flag_interesting.sql
+++ models/prep/prep_flag_interesting.sql
@@ -5,28 +5,31 @@ with base as (
     from {{ ref('stg_youtube_trending') }}
 ),
 
+
 flags as (
     select
         *,
-        view_count > 1000000 as is_viral
+        view_count > 1_000_000 as is_viral,
+        like_count > 100_000 as is_highly_liked,
+        comment_count > 50_000 as is_highly_commented,
+        duration_seconds > 600 as is_long_form
     from base
-),
-
-region_counts as (
-    select
-        video_id,
-        count(distinct default_language) as region_count
-    from base
-    group by video_id
-),
-
-combined as (
-    select
-        f.*,
-        rc.region_count,
-        rc.region_count > 1 as is_multi_region
-    from flags f
-    left join region_counts rc using (video_id)
 )
 
-select * from combined
+select 
+    video_id,
+    title,
+    channel_title,
+    category_id,
+    published_at,
+    load_date,
+    trending_date,
+    view_count,
+    like_count,
+    comment_count,
+    duration_seconds,
+    is_viral,
+    is_highly_liked,
+    is_highly_commented,
+    is_long_form
+from flags
